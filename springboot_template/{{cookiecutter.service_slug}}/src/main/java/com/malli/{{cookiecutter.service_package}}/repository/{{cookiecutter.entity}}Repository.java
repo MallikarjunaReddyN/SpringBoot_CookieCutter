1e04c5177eeecdc737b7442d98b401a1bfc0268a
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2021-09-07 22:42:28 +0530

    Update {{cookiecutter.entity}}Repository.java

--- springboot_template/{{cookiecutter.service_slug}}/src/main/java/com/malli/{{cookiecutter.service_package}}/repository/{{cookiecutter.entity}}Repository.java
+++ springboot_template/{{cookiecutter.service_slug}}/src/main/java/com/malli/{{cookiecutter.service_package}}/repository/{{cookiecutter.entity}}Repository.java
@@ -1,6 +1,6 @@
 package com.malli.{{ cookiecutter.service_package }}.repository;
 
-import com.malli.demoservice.domain.{{ cookiecutter.entity }};
+import com.malli.{{ cookiecutter.service_package }}.domain.{{ cookiecutter.entity }};
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
