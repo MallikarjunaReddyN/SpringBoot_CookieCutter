b665296d4a8edd8921a91790abc06f2d71196871
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2021-09-07 22:41:54 +0530

    Update {{cookiecutter.entity}}Controller.java

--- springboot_template/{{cookiecutter.service_slug}}/src/main/java/com/malli/{{cookiecutter.service_package}}/controller/{{cookiecutter.entity}}Controller.java
+++ springboot_template/{{cookiecutter.service_slug}}/src/main/java/com/malli/{{cookiecutter.service_package}}/controller/{{cookiecutter.entity}}Controller.java
@@ -3,7 +3,7 @@ package com.malli.{{ cookiecutter.service_package }}.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.malli.demoservice.service.{{ cookiecutter.entity }}Service;
+import com.malli.{{ cookiecutter.service_package }}.service.{{ cookiecutter.entity }}Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
