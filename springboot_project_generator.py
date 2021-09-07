daf9ed2a443b8557128e9111a543e17284a96931
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2021-09-07 22:36:20 +0530

    Update springboot_project_generator.py

--- springboot_project_generator.py
+++ springboot_project_generator.py
@@ -1,8 +1,9 @@
 from cookiecutter.main import cookiecutter
 import random
 
-service_port=random.randint(1000, 9999)
+service_port = random.randint(1000, 9999)
 
-cookiecutter('.\springboot_template',
-            extra_context={service_port':service.port}
+cookiecutter(
+		'.\springboot_template',
+        extra_context = {'service_port': service_port}
 ) 
