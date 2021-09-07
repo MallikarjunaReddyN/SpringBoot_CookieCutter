db2e1d200d5fbc5b33f5114bd1e8b14fe6e0650a
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2021-09-07 22:29:26 +0530

    Update {{cookiecutter.entity}}Repository.java

--- springboot_template/{{cookiecutter.service_slug}}/src/main/java/com/malli/{{cookiecutter.service_package}}/repository/{{cookiecutter.entity}}Repository.java
+++ springboot_template/{{cookiecutter.service_slug}}/src/main/java/com/malli/{{cookiecutter.service_package}}/repository/{{cookiecutter.entity}}Repository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class {{ cookiecutter.entity }}Repository extends JpaRepository<{{ cookiecutter.entity }}, Integer> {
+public interface {{ cookiecutter.entity }}Repository extends JpaRepository<{{ cookiecutter.entity }}, Integer> {
 	
 }
